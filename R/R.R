f0ebc9fe91bfbc148daaa74216abb3a6d4a83441
Martin Smith <martins@gmail.com>
2020-09-13 20:36:36 +0100

    NAMESPACE update

--- R/R.R
+++ R/R.R
@@ -1,4 +1,6 @@
 #' @useDynLib ExportTest, .registration = TRUE
+#' @importFrom Rcpp evalCpp
+#' @exportPattern "^[[:alpha:]]+"
 .onUnload <- function (libpath) {
   library.dynam.unload("ExportTest", libpath)
 }
